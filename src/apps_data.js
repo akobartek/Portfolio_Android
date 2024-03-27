b0b746cf1e287ad6d4fb5d936cdd7e10a9bcda8a
Bartłomiej Sokołowski <sokolowskijbartek@gmail.com>
2024-03-27 16:24:24 +0100

    Update apps_data.js

--- src/apps_data.js
+++ src/apps_data.js
@@ -5,42 +5,42 @@ import MFTauIc from "./images/ic_mftau.png";
 import FlatAppIc from "./images/ic_flatapp.png";
 
 export const titles = [
+  "SM Wołczyn",
+  "MF Tau",
   "dictionARy",
   "Marta",
   "FlatApp",
   // "Naprawiamy",
-  "SM Wołczyn",
-  "MF Tau",
 ];
 export const descriptions = [
-  "Application for learning foreign languages by learning the most commonly used words. It has also an Augmented Reality module for learning names of objects that user can scan in the app. Made as part of my Master Thesis.",
-  "Application made in a team of two, as part of the hackathon Hack The Crisis Denmark. It serves to support volunteer teams and was created to fight the effects of the coronavirus epidemic.",
-  "Application that facilitates the management of duties in the apartment. It enables quick contact of roommates and allows creating joint lists of products and responsibilities.",
-  // "Application for sharing your skills with other people. You can search for a specialist for some work as a customer or take a job to make some money as a specialist.",
   "Application created for the needs of the Youth Meeting in Wołczyn, in use since 2019, supporting the organization of the meeting and enabling the sharing of materials.",
   "Application created for the community named MF Tau to help manage the community and organize the meetings with built-in materials. Made as part of my BSc Thesis.",
+  "Application for learning foreign languages by learning the most commonly used words. It has also an Augmented Reality module for learning names of objects that user can scan in the app. Made as part of my Master Thesis.",
+  "Application made in a team of two, as part of the hackathon Hack The Crisis Denmark. It serves to support volunteer teams and was created to fight the effects of the coronavirus epidemic.",
+  "Application that facilitates the management of duties in the apartment. It enables quick contact of roommates and allows creating joint lists of products and responsibilities. Unfortunately, it's no longer maintained.",
+  // "Application for sharing your skills with other people. You can search for a specialist for some work as a customer or take a job to make some money as a specialist.",
 ];
 export const icons = [
+  WolczynIc,
+  MFTauIc,
   DictionaryIc,
   MartaIc,
   FlatAppIc,
   // NaprawiamyIc,
-  WolczynIc,
-  MFTauIc,
 ];
 export const githubLinks = [
+  "https://github.com/akobartek/SMWolczyn",
+  "https://github.com/akobartek/MFTau_App",
   "https://github.com/akobartek/dictionARy",
   "https://github.com/akobartek/Marta_Android",
   "",
   // "https://github.com/akobartek/Naprawiamy",
-  "https://github.com/akobartek/SMWolczyn",
-  "https://github.com/akobartek/MFTau_App",
 ];
 export const downloadLinks = [
-  "https://drive.google.com/file/d/1FRruoWge5EGIo52resLdBPq4Rkc5pUy_/view?usp=sharing",
-  "https://drive.google.com/file/d/1SVRNoXnhqAQddPZTWpVRmnIi19DiJy7q/view?usp=sharing",
-  "https://play.google.com/store/apps/details?id=com.hfad.flatapp",
-  // "",
   "https://play.google.com/store/apps/details?id=pl.kapucyni.wolczyn.app",
   "https://play.google.com/store/apps/details?id=pl.mftau.mftau",
+  "https://drive.google.com/file/d/1FRruoWge5EGIo52resLdBPq4Rkc5pUy_/view?usp=sharing",
+  "https://drive.google.com/file/d/1SVRNoXnhqAQddPZTWpVRmnIi19DiJy7q/view?usp=sharing",
+  "",
+  // "",
 ];
